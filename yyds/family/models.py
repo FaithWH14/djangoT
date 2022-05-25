cb2f71763f4329ad562e558f0def1278d1f8b3be
WenHao <105737893+FaithWH14@users.noreply.github.com>
2022-05-25 11:51:00 +0800

    Delete yyds directory

--- yyds/family/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class familyINFO(models.Model):
-    name = models.CharField(max_length = 20)
-    age = models.IntegerField()
-    salary = models.DecimalField(decimal_places = 2, max_digits = 10)
-    relationship = models.CharField(max_length = 10)
